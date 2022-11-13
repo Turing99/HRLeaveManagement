7ff871dd72034117ee29798bb4a137055129b49c
Turing99 <andrei.fratila99@yahoo.ro>
2022-11-13 23:34:47 +0200

    Add project files.

---
+++ HRLeaveManagement.Domain/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRLeaveManagement.Domain
+{
+    public class LeaveType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string DefaultDays { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+
+    }
+}
